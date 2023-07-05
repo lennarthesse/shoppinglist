5eea6bedc898e3f7866328414cc5dd0a542aab33
lennarthesse <lennart.hesse1301@gmail.com>
2023-07-05 16:02:36 +0200

    Added most of the html and css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,131 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+    <h1>ShoppingList</h1>
+    <div class="items">
+        <h2>Einkaufszettel</h2>
+        <ul>
+            <li class="item">
+                <input type="checkbox" id="item1">
+                <label for="item1">Item 1</label>
+                <button class="remove-btn">
+                    ✕
+                </button>
+            </li>
+            <li class="item">
+                <input type="checkbox" id="item2">
+                <label for="item2">Item 2</label>
+                <button class="remove-btn">
+                    ✕
+                </button>
+            </li>
+            <li class="item">
+                <input type="checkbox" id="item3">
+                <label for="item3">Item 3</label>
+                <button class="remove-btn">
+                    ✕
+                </button>
+            </li>
+            <li class="item">
+                <input type="checkbox" id="item4">
+                <label for="item4">Item 4</label>
+                <button class="remove-btn">
+                    ✕
+                </button>
+            </li>
+        </ul>
+    </div>
+
+    <div class="categories">
+        <div class="category">Category 1</div>
+        <div class="category">Category 2</div>
+        <div class="category">Category 3</div>
+        <div class="category">Category 4</div>
+        <div class="category">Category 5</div>
+        <div class="category">Category 6</div>
+    </div>
+
+    <div class="products">
+        <ul>
+            <li class="product">
+                <div class="product-body">
+                    Product 1
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 2
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 3
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 4
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 5
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 6
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 7
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 8
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+            <li class="product">
+                <div class="product-body">
+                    Product 9
+                    <button class="product-info">
+                        ?
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<button class="add-product">
+    <svg viewBox="0 0 100 100"
+        stroke="#ffffff"
+        stroke-width="10">
+        <line x1="50" y1="0" x2="50" y2="100" />
+        <line x1="0" y1="50" x2="100" y2="50" />
+    </svg>
+</button>
